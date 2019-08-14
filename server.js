4cc5a0ccd7e38eef86da3fbbe0708b64b1d5dc8a
Payton Burr <greatpayton@gmail.com>
2019-08-13 23:09:54 -0600

    added routing

---
+++ server.js
@@ -0,0 +1,14 @@
+let express = require('express');
+
+let PORT = process.env.PORT || 8080;
+
+let app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require('./app/routing/htmlRoutes.js')(app);
+
+app.listen(PORT, function() {
+  console.log('App listening on PORT: ' + PORT);
+});
